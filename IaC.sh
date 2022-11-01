b8665113415d40a355dd57f51b9b26fdf9cded7e
Wilian <wiliankcoal@hotmail.com>
2022-11-01 12:32:54 +0000

    Arquivos Infraestrutura as a code v1

---
+++ IaC.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "Criando diretórios..."
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos..."
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuário..."
+useradd carlos -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd maria -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd joao -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd debora -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd roberto -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123)
+
+echo "Adicionando usuários aos grupos..."
+usermod -G GRP_ADM carlos
+usermod -G GRP_ADM maria
+usermod -G GRP_ADM joao
+usermod -G GRP_VEN debora
+usermod -G GRP_VEN sebastiana
+usermod -G GRP_VEN roberto
+usermod -G GRP_SEC josefina
+usermod -G GRP_SEC amanda
+usermod -G GRP_SEC rogerio
+
+echo "Permissões de diretórios..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico 
+
+echo "Fim..." 
+
+
+
+
